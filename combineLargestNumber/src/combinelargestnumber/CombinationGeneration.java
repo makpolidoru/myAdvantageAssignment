72523971bb6b968a559dad0b4765d3dc4da852be
makpolidoru <mak.polidoru@gmail.com>
2018-01-25 23:01:44 +0200

    Formatted

--- combineLargestNumber/src/combinelargestnumber/CombinationGeneration.java
+++ combineLargestNumber/src/combinelargestnumber/CombinationGeneration.java
@@ -5,61 +5,67 @@ import java.util.Collections;
 import java.util.Comparator;
 
 /**
- * Class used for generating the largest possible concatenation of given integers.
+ * Class used for generating the largest possible concatenation of given
+ * integers.
+ * 
  * @author makpo
  *
  */
 public class CombinationGeneration {
-	
+
 	private ArrayList<Integer> listOfNumbers;
-	
+
 	private Comparator<Integer> sorter = new IntegerComparator();
-	
+
 	public CombinationGeneration(ArrayList<Integer> listInput) {
-		listOfNumbers =  listInput;
+		listOfNumbers = listInput;
 	}
-	
+
 	private class IntegerComparator implements Comparator<Integer> {
-        @Override
-        /*Implement Comparator ; custom ordering is needed*/
-        public int compare(Integer in1, Integer in2) {
-            String str1 = in1.toString();
-            String str2 = in2.toString();
- 
-            //if the 2 numbers appear to be of the same length, then compare to keep the larger one.
-            if(str1.length() == str2.length()){
-                return str2.compareTo(str1);
-            }
-            else {
- 
-            	String initial1 = str1;
-            	String initial2 = str2;
-	            //if they are not, we need to keep the one with the greater 1st digit, no matter the length.
-	            //To compare, we need to bring them on the same length.
-	            int max_length = Math.max(str1.length(), str2.length());
-	            while(str1.length() < max_length * 2) str1 += initial1;
-	            while(str2.length() < max_length * 2) str2 += initial2;
-	 
-	            return str2.compareTo(str1);
-            }
-        }
-    };
-	
+		@Override
+		/* Implement Comparator ; custom ordering is needed */
+		public int compare(Integer in1, Integer in2) {
+			String str1 = in1.toString();
+			String str2 = in2.toString();
+
+			// if the 2 numbers appear to be of the same length, then compare to keep the
+			// larger one.
+			if (str1.length() == str2.length()) {
+				return str2.compareTo(str1);
+			} else {
+
+				String initial1 = str1;
+				String initial2 = str2;
+				// if they are not, we need to keep the one with the greater 1st digit, no
+				// matter the length.
+				// To compare, we need to bring them on the same length.
+				int max_length = Math.max(str1.length(), str2.length());
+				while (str1.length() < max_length * 2)
+					str1 += initial1;
+				while (str2.length() < max_length * 2)
+					str2 += initial2;
+
+				return str2.compareTo(str1);
+			}
+		}
+	};
+
 	/**
-	 *This is a method that generates and returns the largest number combining the given integers
-	 *@input : the list of non negative integers
-	 *@return : combination of integers that give the largest result as String.
+	 * This is a method that generates and returns the largest number combining the
+	 * given integers
+	 * 
+	 * @input : the list of non negative integers
+	 * @return : combination of integers that give the largest result as String.
 	 */
 	protected String generateLargestCombinationOfNumbers() {
-	    String concatenatedCombo = "";
-	    Collections.sort(listOfNumbers, sorter);
-	    //concatenate in one string value :
-        for(Integer token:listOfNumbers){
-        	concatenatedCombo += token;
-        }
-       return concatenatedCombo;
-       
-    }
-	   
+		String concatenatedCombo = "";
+		Collections.sort(listOfNumbers, sorter);
+		// concatenate in one string value :
+		for (Integer token : listOfNumbers) {
+			concatenatedCombo += token;
+		}
+		return concatenatedCombo;
+
+	}
 
 }
--- combineLargestNumber/src/combinelargestnumber/CombineLargestNumberLauncher.java
+++ combineLargestNumber/src/combinelargestnumber/CombineLargestNumberLauncher.java
@@ -1,76 +1,80 @@
 package combinelargestnumber;
+
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class CombineLargestNumberLauncher {
 
 	public static void main(String[] args) {
-		
+
 		boolean runAgain = true;
 		Scanner scanner = new Scanner(System.in);
-		scanner.useDelimiter(","); 
-		
+		scanner.useDelimiter(",");
+
 		while (runAgain) {
-			
+
 			boolean isLegitNumber = true;
 			ArrayList<Integer> listNumbers = new ArrayList<Integer>();
-					
-			System.out.println(" > Please provide a list of non negative integers, separated by comma. \nPress enter when finished.");
-		
-				String[] inputNumbers = scanner.nextLine().split(",");
-				String inputNumber; 
-				int position = 0;
-				
-				while (isLegitNumber && position<inputNumbers.length) {
-					inputNumber = inputNumbers[position].trim();
-					if(inspectElements(inputNumber)) { 
-						 listNumbers.add(Integer.parseInt(inputNumber));
-						 position++;
-					}
-					else {
-						 isLegitNumber = false;
-						 listNumbers.clear();
-					}
-						
-				}		    
-	
-			    if (isLegitNumber) {
-			    	CombinationGeneration computeCombo = new CombinationGeneration(listNumbers);
-			    	System.out.println("\nConcatenated combination: "+ computeCombo.generateLargestCombinationOfNumbers());
-			    }
-			    else {
-			    	//if 1 or more numbers were not legit
-			    	System.out.println("Give input properly.");
-			    }	
-			    System.out.println("\n > Try again? (Y/N)");
-		    	String answer = scanner.nextLine();
-		    	if (answer.equalsIgnoreCase("Y")) runAgain = true;
-		    	else runAgain = false;  
-			
+
+			System.out.println(
+					" > Please provide a list of non negative integers, separated by comma. \nPress enter when finished.");
+
+			String[] inputNumbers = scanner.nextLine().split(",");
+			String inputNumber;
+			int position = 0;
+
+			while (isLegitNumber && position < inputNumbers.length) {
+				inputNumber = inputNumbers[position].trim();
+				if (inspectElements(inputNumber)) {
+					listNumbers.add(Integer.parseInt(inputNumber));
+					position++;
+				} else {
+					isLegitNumber = false;
+					listNumbers.clear();
+				}
+
+			}
+
+			if (isLegitNumber) {
+				CombinationGeneration computeCombo = new CombinationGeneration(listNumbers);
+				System.out.println("\nConcatenated combination: " + computeCombo.generateLargestCombinationOfNumbers());
+			} else {
+				// if 1 or more numbers were not legit
+				System.out.println("Give input properly.");
+			}
+			System.out.println("\n > Try again? (Y/N)");
+			String answer = scanner.nextLine();
+			if (answer.equalsIgnoreCase("Y"))
+				runAgain = true;
+			else
+				runAgain = false;
+
 		}
 		scanner.close();
 		System.out.println("\nBye!");
-	} 
-	
+	}
+
 	/**
-	 *This is a method that inspects whether the given numbers meet the requirements,
-	 *those being giving as input a banch of non negative integers
-	 *@param tokenToInspect : every given number
-	 *@return true/false : whether the number is legit or not.
-	 *@throws NumberFormatException
+	 * This is a method that inspects whether the given numbers meet the
+	 * requirements, those being giving as input a banch of non negative integers
+	 * 
+	 * @param tokenToInspect
+	 *            : every given number
+	 * @return true/false : whether the number is legit or not.
+	 * @throws NumberFormatException
 	 */
 	public static boolean inspectElements(String tokenToInspect) {
-		 try {
-	            Integer.parseInt(tokenToInspect);
-	            if (Integer.parseInt(tokenToInspect) < 0) {
-	            	System.out.println("Only positive Integers should be inserted! "+tokenToInspect + " is not a positive integer");
-	            	return false;
-	            }   
-	        } 
-	        catch (NumberFormatException e) {
-				System.out.println(tokenToInspect + " is not a valid integer number");
+		try {
+			Integer.parseInt(tokenToInspect);
+			if (Integer.parseInt(tokenToInspect) < 0) {
+				System.out.println(
+						"Only positive Integers should be inserted! " + tokenToInspect + " is not a positive integer");
 				return false;
-	        }
+			}
+		} catch (NumberFormatException e) {
+			System.out.println(tokenToInspect + " is not a valid integer number");
+			return false;
+		}
 		return true;
 	}
 }
